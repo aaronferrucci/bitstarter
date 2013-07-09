5b7393667cb8a76b70af2e7f1292b849e179a7bb
John Smith <example@stanford.edu>
2013-07-09 06:51:38 +0000

    use index.html

--- web.js
+++ web.js
@@ -2,9 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var buf = fs.readFileSync("index.html");
+var string = buf.toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Goodbye, Cruel World!');
+  response.send(string);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
